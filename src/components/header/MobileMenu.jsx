e0328f3e7440cdd04c5092d6efbde4af342acc07
Amit <vishamit8827@gmail.com>
2024-10-17 17:49:47 +0530

    react

--- src/components/header/MobileMenu.jsx
+++ src/components/header/MobileMenu.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import './MobileMenu.css';
 import logo from "../media/heroDev.png";
 import AlignHorizontalLeftIcon from '@mui/icons-material/AlignHorizontalLeft';
+import {Link}  from "react-router-dom";
 
 const MobileMenu = () => {
     const [menuOpen, setMenuOpen] = useState(false);
@@ -14,7 +15,7 @@ const MobileMenu = () => {
         <div id="mobileMenu" className="mobile-menu">
 
             <div className="mobOne">
-                <img src={logo} alt="" />
+                <Link to="/"><img src={logo} alt="" /></Link>
             </div>
 
             <div className="mobTwo">
@@ -25,10 +26,10 @@ const MobileMenu = () => {
 
                 {menuOpen && (
                     <ul className="menu-items">
-                        <li><a href="#home">Home</a></li>
-                        <li><a href="#about">About</a></li>
-                        <li><a href="#services">Services</a></li>
-                        <li><a href="#contact">Contact</a></li>
+                        <li><Link to="/">Home</Link></li>
+                        <li><Link to="/skills">Skills</Link></li>
+                        <li><Link to="/about">About</Link></li>
+                        <li><Link to="/contact">Contact</Link></li>
                     </ul>
                 )}
             </div>
