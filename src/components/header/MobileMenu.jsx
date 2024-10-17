e72c3a74b44e8b47372eefbcd4dacf81d516c7b5
Amit <vishamit8827@gmail.com>
2024-10-17 18:04:53 +0530

    react

--- src/components/header/MobileMenu.jsx
+++ src/components/header/MobileMenu.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import './MobileMenu.css';
 import logo from "../media/heroDev.png";
 import AlignHorizontalLeftIcon from '@mui/icons-material/AlignHorizontalLeft';
-import {Link}  from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const MobileMenu = () => {
     const [menuOpen, setMenuOpen] = useState(false);
@@ -11,31 +11,31 @@ const MobileMenu = () => {
         setMenuOpen(!menuOpen);
     };
 
+    // Function to close the menu when a link is clicked
+    const closeMenu = () => {
+        setMenuOpen(false);
+    };
+
     return (
         <div id="mobileMenu" className="mobile-menu">
-
             <div className="mobOne">
-                <Link to="/"><img src={logo} alt="" /></Link>
+                <Link to="/"><img src={logo} alt="Logo" /></Link>
             </div>
 
             <div className="mobTwo">
-                
                 <button className="menu-toggle" onClick={toggleMenu}>
-                <AlignHorizontalLeftIcon fontSize="large" sx={{ color: 'white', marginTop: '-19px' }} />
+                    <AlignHorizontalLeftIcon fontSize="large" sx={{ color: 'white', marginTop: '-19px' }} />
                 </button>
 
                 {menuOpen && (
                     <ul className="menu-items">
-                        <li><Link to="/">Home</Link></li>
-                        <li><Link to="/skills">Skills</Link></li>
-                        <li><Link to="/about">About</Link></li>
-                        <li><Link to="/contact">Contact</Link></li>
+                        <li><Link to="/" onClick={closeMenu}>Home</Link></li>
+                        <li><Link to="/skills" onClick={closeMenu}>Skills</Link></li>
+                        <li><Link to="/about" onClick={closeMenu}>About</Link></li>
+                        <li><Link to="/contact" onClick={closeMenu}>Contact</Link></li>
                     </ul>
                 )}
             </div>
-
-
-
         </div>
     );
 };
