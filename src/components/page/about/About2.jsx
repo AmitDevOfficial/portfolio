ff44b46267f51ba6a3dd716e5256179a2a6fd04c
Amit <vishamit8827@gmail.com>
2024-10-18 11:41:49 +0530

    react

--- src/components/page/about/About2.jsx
+++ src/components/page/about/About2.jsx
@@ -13,7 +13,7 @@ export default function About2() {
             <p><b>10th Higher Secondary</b></p>
             <ul>
               <li>School for Excellence Dewas (Madhya Pradesh)</li>
-              <li><b>(2011 - 2012)</b></li>
+              <li className='secondli'><b>(2011 - 2012)</b></li>
             </ul>
           </div>
         </div>
@@ -23,7 +23,7 @@ export default function About2() {
             <p><b>12th Higher Secondary</b></p>
             <ul>
               <li>S.V.M Dewas (Madhya Pradesh)</li>
-              <li><b>(2011 - 2012)</b></li>
+              <li className='secondli'><b>(2011 - 2012)</b></li>
             </ul>
           </div>
         </div>
@@ -34,7 +34,7 @@ export default function About2() {
             <p><b>Bachlor's of Computer Application ( BCA )</b></p>
             <ul>
               <li>Prestige Institute of Mangment Dewas (Madhya Pradesh)</li>
-              <li><b>(2011 - 2012)</b></li>
+              <li className='secondli'><b>(2011 - 2012)</b></li>
             </ul>
           </div>
         </div>
@@ -45,7 +45,7 @@ export default function About2() {
             <p><b>Master's of Computer Application ( MCA )</b></p>
             <ul>
               <li>Sage University Indore (Madhya Pradesh)</li>
-              <li><b>(2011 - 2012)</b></li>
+              <li className='secondli'><b>(2011 - 2012)</b></li>
             </ul>
           </div>
         </div>
